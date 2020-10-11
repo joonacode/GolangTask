d665cb3e907afc67d11c56624db385b9ff34477e
joonacode <joonacode@gmail.com>
2020-10-11 16:15:48 +0700

    add task1

---
+++ task1.go
@@ -0,0 +1,38 @@
+package main
+
+// Cetak gambar
+
+import (
+	"fmt"
+)
+
+func cetakGambar(param int) {
+	if param % 2 == 0 {
+		fmt.Println("Parameter harus bilangan ganjil")
+	} else{
+		var penengah = param - ((param - 1) / 2)
+		for i := 1; i <= param; i++ {
+			var tampung string = ""
+			for x := 1; x <= param; x++{
+				if i == penengah {
+					tampung += "* "
+				}else{
+					if x == 1 || x == param{
+						tampung += "* "
+					}else{
+						tampung += "= "
+					}
+				}
+			}
+			fmt.Println(tampung)
+		}	
+	}
+	fmt.Println("")
+}
+
+func main() {
+	cetakGambar(5)
+	cetakGambar(10)
+	cetakGambar(3)
+	cetakGambar(9)
+}
